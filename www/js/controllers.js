ada87b30b8bc23626c6f8d1466384b861648b5dd
Emarie <emariecarbajosa@gmail.com>
2017-02-15 01:01:32 +0800

    fetching of data from server done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,15 @@
 angular.module('app.controllers', [])
   
-.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams', '$http', '$rootScope',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
+function ($scope, $stateParams, $http, $rootScope) {
+	$http.post('https://anilist.co/api/auth/access_token', {grant_type : "client_credentials",
+		client_id : 'nerdykhaleesi25-gnvdr',
+		client_secret : 'fuC8tGrRA6tp7o9crbA7'}).then(function(response){
+			$rootScope.access_token = response.data.access_token;
+			console.log($rootScope.access_token);
+		});
 
 }])
    
@@ -16,11 +21,25 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('animeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('animeCtrl', ['$scope', '$stateParams', '$http', '$rootScope', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
+function ($scope, $stateParams, $http, $rootScope) {
+	if(!$rootScope.access_token){
+		$http.post('https://anilist.co/api/auth/access_token', {grant_type : "client_credentials",
+		client_id : 'nerdykhaleesi25-gnvdr',
+		client_secret : 'fuC8tGrRA6tp7o9crbA7'}).then(function(response){
+			$rootScope.access_token = response.data.access_token;
+			console.log($rootScope.access_token);
+		});
+	}
+	
+	
+	$http.get('http://anilist.co/api/browse/anime?sort=popularity-desc&year=2017&season=Winter&airing_data=true&page=1&access_token='+$rootScope.access_token).then(function(response){
+		console.log($scope.access_token);
+		$scope.anime = response.data;
+		console.log($scope.anime);
+	});
 
 }])
       
